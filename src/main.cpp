297aa7f9e01a69078030ed798e0a2f5b5facc481
Carlos Eduardo Silva Almeida <seu-email@exemplo.com>
2025-08-03 14:59:27 -0300

    Criacao da grid

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <raylib.h>
+#include "grid.h"
 
 int main() 
 {
@@ -6,10 +7,18 @@ int main()
     Color darkBlue = {44, 44, 127, 255};
     InitWindow(300, 600, "raylib Tetris");
     SetTargetFPS(60); // Define quantas vezes o loop principal roda por segundo
+
+    Grid grid = Grid();
+    grid.grid[0][0] = 1;
+    grid.grid[3][5] = 4;
+    grid.grid[17][8] = 7;
+    grid.Print();
+
     while(WindowShouldClose() == false){ // verifica se ESC ou sair foi precionado
 
         BeginDrawing(); // Desenha uma tela branca
         ClearBackground(darkBlue);
+        grid.Draw();
 
 
         EndDrawing(); // Fecha a tela
